7abe2a789ad8baed1ee07a9b1881e4c41606b564
Ya <652111934@qq.com>
2020-12-16 22:03:03 +0800

    初始化基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid" style="height: 100%">
+    <header class="row"></header>
+    <section class="row" id="main_body">
+      <div id="left" class="col-2 "></div>
+      <div id="main" class="col-8 "></div>
+      <div id="right" class="col-2"></div>
+    </section>
+    <footer class="row"></footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+header {
+  height: 10%;
+}
+#main_body {
+  height: 80%;
+}
+
+#main {
+    background: gray;
+}
+footer {
+  height: 10%;
+
 }
 </style>
